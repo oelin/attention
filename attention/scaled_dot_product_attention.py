839088ba2326ef28208afe9b83bf68b372bfa0f2
Kale Champagnie <42823429+oelin@users.noreply.github.com>
2023-09-29 12:29:44 +0100

    Create scaled_dot_product_attention.py

---
+++ attention/scaled_dot_product_attention.py
@@ -0,0 +1,7 @@
+"""Scaled Dot-product Attention.
+
+This implementation is based on PyTorch's `scaled_dot_product_attention` function, which takes in
+precomputed query, key and value tensors as well as an optional mask tensor. If `is_causal` is set
+
+See:
+See: 
