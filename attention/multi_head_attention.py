aa7ed61fae335f20ac260538a9b6fd2cc9d45892
Kale Champagnie <42823429+oelin@users.noreply.github.com>
2023-09-30 09:12:20 +0100

    Update multi_head_attention.py

--- attention/multi_head_attention.py
+++ attention/multi_head_attention.py
@@ -1,5 +1,55 @@
+from typing import Optional
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from attention.scaled_dot_product_attention import 
+from attention.scaled_dot_product_attention import scaled_dot_product_attention, ScaledDotProductAttention
+
+
+class SlowMultiHeadAttention(nn.Module):
+    """Slow multi-head attention. 
+
+    Uses `number_of_heads` ScaledDotProductAttention layers to compute the output
+    of each head, then concatenates them.
+    """
+    
+    def __init__(
+        self, 
+        number_of_heads: int, 
+        head_dimension: int, 
+        output_dimension: int,
+    ) -> None:
+        super().__init__()
+
+        self.linear_output = nn.Linear(
+            head_dimension * number_of_heads, 
+            output_dimension, 
+            bias=False,
+        )
+        
+        self.heads = nn.ModuleList([
+            ScaledDotProductAttention(
+                query_dimension=head_dimension, 
+                value_dimension=head_dimension,
+            )
+
+            for _ in range(number_of_heads)
+        ])
+
+    def forward(
+        self, 
+        x: torch.Tensor, 
+        mask: Optional[torch.Tensor] = None,
+    ) -> torch.Tensor:
+
+        x = torch.cat([head(x, mask) for head in self.heads], dim=-1)
+        x = self.linear_output(x)
+
+        return x
+
+
+class MultiHeadAttention(nn.Module):
+    """Efficient multi-head attention."""
+
+    ...
